709bf856200a57efd9bcbb9fe7a006caad36bcfa
Yarch <yarchyk12@gmail.com>
2026-06-14 16:21:35 -0400

    non finished commit, in the middle of moving and commiting working changes, started implementing default main game scene and template for levels

--- src/core/main_game/main.gd
+++ src/core/main_game/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var bullet_manager = $BulletManager
+@onready var bullet_manager: Node2D = $Systems/BulletManager
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,5 +17,5 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
